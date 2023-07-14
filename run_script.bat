5d9e110d95b8c06bbb82cdd20d26ed811e5dbe6a
snailz <adamlancaster.al@gmail.com>
2023-07-14 19:54:53 -0400

    added bat script for windows.

---
+++ run_script.bat
@@ -0,0 +1,7 @@
+:: this script will run a python program ('main.py') using the local virtual environment (.env) on windows 10.
+:: note that the venv must be generated in the same folder as the python script. 
+
+@echo off
+call .env\Scripts\activate.bat
+python main.py
+call .env\Scripts\deactivate.bat
