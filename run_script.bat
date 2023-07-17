8f04d2a07acc67c2d2383f8821f4ab39768a3599
snailz <adamlancaster.al@gmail.com>
2023-07-17 16:09:52 -0400

    updated comments.

--- run_script.bat
+++ run_script.bat
@@ -1,5 +1,6 @@
-:: this script will run a python program ('main.py') using the local virtual environment (.env) on windows 10.
-:: note that the venv must be generated in the same folder as the python script. 
+:: this script will run a python program ('main.py') using the local virtual 
+:: environment (.env) on windows 10. note that the venv must be generated in 
+:: the same folder as the python script. 
 
 @echo off
 call .env\Scripts\activate.bat
